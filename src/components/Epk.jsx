b99aedec1ca504ce7bb4679ce596a66f3a89cdd2
sofiamchugh <sc21sm2@leeds.ac.uk>
2024-07-10 21:30:34 +0100

    Added videos to EPK

--- src/components/Epk.jsx
+++ src/components/Epk.jsx
@@ -5,14 +5,14 @@ const Epk = () => {
         <main>
         <div style = {{height: 150 }}></div>
         <div className="page">
-      
-        <p className ={styles.bio}>Sofia and the Spectrum began as a joke. <br></br> <br></br>
+      <div className = "box box1">
+        <p className ={styles.bio} >Sofia and the Spectrum began as a joke. <br></br> <br></br>
 
 “If I fronted a band, I’d call it Sofia and the Spectrum”, said Sofia, because she’s on the spectrum. Everyone laughed. She laughed.
 <br></br><br></br>
 And then she thought a little harder about it. 
 <br></br><br></br>
-Sofia and the Spectrum is, above all, music for people who think too hard about things. It’s what happens when a steady diet of confessional singer-songwriter pop lyricism meets a lifelong fascination with the blues and its relatives. Every song is full of visceral, theatrical intensity, the product of ruminating too long over every little thought and emotion, paper and pen in hand, and the band behind it only adds to that intensity. Freewheeling through the decades from classic blues to rock and roll to modern blues rock, the band’s sound is dynamic, infectious, and <i>big</i>. 
+Sofia and the Spectrum is music for people who think too hard about things. It’s what happens when a steady diet of confessional singer-songwriter pop lyricism meets a lifelong fascination with the blues and its relatives. Every song is full of visceral, theatrical intensity, the product of ruminating too long over every little thought and emotion, paper and pen in hand, and the band behind it only adds to that intensity. Freewheeling through the decades from classic blues to rock and roll to modern blues rock, the band’s sound is dynamic, infectious, and <i>big</i>. 
 <br></br><br></br>
 
 <i>Introducing...</i>
@@ -25,6 +25,14 @@ Sofia and the Spectrum is, above all, music for people who think too hard about 
     <li>Viv on trumpet</li> <br></br><br></br><br></br>
 </ul>
 </p>
+<iframe title = "go" width="420" height="315"
+src="https://youtube.com/embed/f0Tqf8pI_Rs">
+</iframe>
+<iframe title = "pos" width="420" height="315"
+src="https://youtube.com/embed/3huHKGMSJuE">
+</iframe>
+
+        </div>
         </div>
         </main>
     );
