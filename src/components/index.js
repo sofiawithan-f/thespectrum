5159ee45bf980dc7944032bec55f271176e9067e
sofiamchugh <sc21sm2@leeds.ac.uk>
2024-07-10 18:22:49 +0100

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from '../reportWebVitals';
-
-export { default as Navigation } from "./Navigation";
-export { default as Home } from "./Home";
-export { default as Epk } from "./Epk";
-export { default as Contact } from "./Contact";
